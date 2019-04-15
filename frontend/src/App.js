bea43405f754d76390400033d4e4df2d926a1caf
sibelly <sibellychan@hotmail.com>
2019-04-15 09:40:47 -0400

    Adds the new question form and the silent auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 
 import NavBar from './NavBar/NavBar';
 import Question from './Question/Question';
 import Questions from './Questions/Questions';
 import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
+import auth0Client from './Auth';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+
+  async componentDidMount() {
+
+    if (this.props.location.pathname === '/callback') {
+      this.setState({checkingSession:false});
+      return;
+    }
+
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({checkingSession:false});
+  }
+
   render() {
     return (
       <div>
@@ -14,9 +41,12 @@ class App extends Component {
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
         <Route exact path='/callback' component={Callback}/>
+        <SecuredRoute path='/new-question'
+                  component={NewQuestion}
+                  checkingSession={this.state.checkingSession} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
