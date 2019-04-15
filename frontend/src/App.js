5c36ee8de3f1aca3c0899d4e9927d50db79ed0ec
sibelly <sibellychan@hotmail.com>
2019-04-15 01:07:50 -0400

    Adds the NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import NavBar from './NavBar/NavBar';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar/>
         <p>Work in progress.</p>
       </div>
     );
